c2746501900b39f4e4f1cf3cd1c14213a229dce3
Leigh Nystrom <nystrom@princeton.edu>
2020-11-04 16:11:04 -0500

    fixing .simg typo

--- preprocessing/run_mriqc.sh
+++ preprocessing/run_mriqc.sh
@@ -6,7 +6,7 @@ singularity run --cleanenv \
     --bind $bids_dir:/home \
     --bind $scratch_dir:/scratch \
     --bind /usr/people \
-    /jukebox/hasson/singularity/mriqc/mriqc-v0.15.1.sqsh \
+    /jukebox/hasson/singularity/mriqc/mriqc-v0.15.1.simg \
     --participant-label $1 \
     --correct-slice-timing --no-sub \
     --nprocs 8 -w /scratch \
