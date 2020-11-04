782a08c5d00bba080e418c5ca5827b4cae36fa7c
Leigh Nystrom <nystrom@princeton.edu>
2020-11-04 14:39:04 -0500

    update MRIQC version; send work to scratch

--- preprocessing/run_mriqc.sh
+++ preprocessing/run_mriqc.sh
@@ -4,8 +4,10 @@ source globals.sh
 
 singularity run --cleanenv \
     --bind $bids_dir:/home \
-    /jukebox/hasson/singularity/mriqc/mriqc-v0.10.4.sqsh \
-    --participant-label sub-$1 \
+    --bind $scratch_dir:/scratch \
+    --bind /usr/people \
+    /jukebox/hasson/singularity/mriqc/mriqc-v0.15.1.sqsh \
+    --participant-label $1 \
     --correct-slice-timing --no-sub \
-    --nprocs 8 -w /home/derivatives/work \
-    /home /home/derivatives/mriqc participant
+    --nprocs 8 -w /scratch \
+    /home /bids/derivatives/mriqc participant
